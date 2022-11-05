1b1fa5ecbd43cc9bb77509e8ea213e61cbf5ad46
luthfi Amrullah <mluthfiamrullah@gmail.com>
2022-11-06 00:08:37 +0700

    create factory

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,13 @@
 
 namespace Database\Seeders;
 
+use App\Models\Company;
+use App\Models\Team;
+use App\Models\Role;
+use App\Models\Responsibility;
+use App\Models\Employee;
+use App\Models\User;
+use App\Models\UserCompany;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -14,11 +21,18 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        // \App\Models\User::factory(70)->create();
 
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+
+        // Company::factory(10)->create();
+        // Team::factory(30)->create();
+        // Role::factory(50)->create();
+        // Responsibility::factory(200)->create();
+        // Employee::factory(1000)->create();
+        // User::factory(70)->create()
     }
 }
