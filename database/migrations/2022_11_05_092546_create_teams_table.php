837f83ac3679a099aff3f34c70068b7d396ddea0
luthfi Amrullah <mluthfiamrullah@gmail.com>
2022-11-05 17:20:07 +0700

    fix migration

--- database/migrations/2022_11_05_092546_create_teams_table.php
+++ database/migrations/2022_11_05_092546_create_teams_table.php
@@ -15,7 +15,6 @@ return new class extends Migration
     {
         Schema::create('teams', function (Blueprint $table) {
             $table->id();
-            $table->timestamps();
             $table->string('name');
             $table->string('icon')->nullable();
             $table->bigInteger('company_id')->unsigned();
