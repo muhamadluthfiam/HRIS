c3e7db31eaa6b24a5ed88a7ae0c7ffc687e45511
luthfi Amrullah <mluthfiamrullah@gmail.com>
2022-11-05 18:44:22 +0700

    adding softdelete

--- database/migrations/2022_11_05_093324_create_responsibilities_table.php
+++ database/migrations/2022_11_05_093324_create_responsibilities_table.php
@@ -17,6 +17,7 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->bigInteger('role_id')->unsigned();
+            $table->softDeletes();
             $table->timestamps();
         });
     }
