9ac533e6cae1abe3c9cf92755369912d275c5847
luthfi Amrullah <mluthfiamrullah@gmail.com>
2022-11-07 20:20:58 +0700

    CRUD API module team done

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\API\UserController;
 use App\Http\Controllers\API\CompanyController;
+use App\Http\Controllers\API\TeamController;
 
 /*
 |--------------------------------------------------------------------------
@@ -20,6 +21,13 @@ Route::prefix('company')->middleware('auth:sanctum')->name('company.')->group(fu
   Route::get('', [CompanyController::class, 'fetch'])->name('fetch');
   Route::post('', [CompanyController::class, 'create'])->name('create');
   Route::post('update/{id}', [CompanyController::class, 'update'])->name('update');
+});
+
+Route::prefix('team')->middleware('auth:sanctum')->name('team.')->group(function () {
+  Route::get('', [TeamController::class, 'fetch'])->name('fetch');
+  Route::post('', [TeamController::class, 'create'])->name('create');
+  Route::post('update/{id}', [TeamController::class, 'update'])->name('update');
+  Route::delete('{id}', [TeamController::class, 'destroy'])->name('delete');
 });
 
 Route::name('auth.')->group(function () {
