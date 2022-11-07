13c6b93f3e6fdd61e0c8a039889966bc7c6586e3
luthfi Amrullah <mluthfiamrullah@gmail.com>
2022-11-07 18:02:14 +0700

    create company by user

--- app/Http/Controllers/API/CompanyController.php
+++ app/Http/Controllers/API/CompanyController.php
@@ -2,25 +2,33 @@
 
 namespace App\Http\Controllers\API;
 
-use App\Http\Controllers\Controller;
+use Exception;
+use App\Models\User;
+use App\Models\Company;
 use Illuminate\Http\Request;
 use App\Helpers\ResponseFormatter;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
 use App\Http\Requests\CreateCompanyRequest;
-use App\Models\Company;
-use Exception;
+use App\Http\Requests\UpdateCompanyRequest;
 
 class CompanyController extends Controller
 {
-    public function all(Request $request)
+    public function fetch(Request $request)
     {
         $id = $request->input('id');
         $name = $request->input('name');
         $limit = $request->input('limit', 10);
 
+        $companyQuery = Company::with(['users'])->whereHas('users', function ($query) {
+            $query->where('user_id', Auth::id());
+        });
+
         // hris.com/api/company?id=1
         if($id)
         {
-            $company = Company::with(['users'])->find($id);
+            $company = $companyQuery->find($id);
+
             if($company)
             {
                 return ResponseFormatter::success($company);
@@ -29,7 +37,7 @@ class CompanyController extends Controller
             return ResponseFormatter::error('Company not found');
         }
 
-        $companies = Company::with(['users']);
+        $companies = $companyQuery;
 
         if($name) {
             $companies->where('name', 'like', '%' . $name . '%');
@@ -55,6 +63,15 @@ class CompanyController extends Controller
                 'logo' => $request->$path
             ]);
 
+            if(!$company) {
+                throw new Exception('Company not created');
+            }
+
+            $user = User::find(Auth::id());
+            $user->companies()->attach($company->id);
+
+            $company->load('users');
+
             return ResponseFormatter::success($company, 'Company created');
 
         } catch (Exception $error) {
@@ -62,5 +79,30 @@ class CompanyController extends Controller
         }
 
         
+    }
+
+    public function update(UpdateCompanyRequest $request, $id)
+    {
+        try {
+            $company = Company::find($id);
+
+            if(!$company) {
+                throw new Exception('Company not found');
+            }
+
+            if($request->hasFile('logo')) {
+                $path = $request->file('logo')->store('public/logos');
+            }
+
+            $company->update([
+                'name' => $request->name,
+                'logo' => $path
+            ]);
+
+            return ResponseFormatter::success($company, 'Company updates');
+
+        } catch (Exception $e) {
+            return ResponseFormatter::error($e->getMessage(), 500);
+        }
     }
 }
